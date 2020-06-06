b35720e29ac160e07a91469d4d8515f9d07df287
Jadan Morrow <jadanmorrow@outlookcom>
2020-06-06 13:56:48 -0600

    server.js

--- server.js
+++ server.js
@@ -5,26 +5,20 @@ var mongoose = require('mongoose');
 
 var PORT = process.env.PORT || 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger('dev'));
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
+
 app.use(express.static('public'));
 
 var MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
-// Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Handlebars
 app.engine(
   'handlebars',
   exphbs({
@@ -33,11 +27,9 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
-// Routes
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-// Start the server
 app.listen(PORT, function () {
   console.log('App running on port ' + PORT + '!');
 });
